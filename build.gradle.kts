c72d1c7f4bac0bd95a68ee8666c805d18c3fa3ff
perandowind <perandowind@gmail.com>
2026-02-24 15:52:05 +0900

    4 5 6

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,11 +26,14 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.boot:spring-boot-h2console")
+    implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    runtimeOnly ("com.h2database:h2")
 }
 
 tasks.withType<Test> {
