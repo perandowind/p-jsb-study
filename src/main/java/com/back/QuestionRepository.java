8518f74ee5f024812910c0d8907e3c4c2ba6a5be
perandowind <perandowind@gmail.com>
2026-02-27 12:40:02 +0900

    11 12 13

--- src/main/java/com/back/QuestionRepository.java
+++ src/main/java/com/back/QuestionRepository.java
@@ -2,5 +2,9 @@ package com.back;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    /**직접 언급해줘야함 & 메서드 명명규칙을 지켜야함!!*/
+    Optional<Question> findBySubject(String subject);
 }
--- src/test/java/com/back/QuestionRepositoryTests.java
+++ src/test/java/com/back/QuestionRepositoryTests.java
@@ -1,5 +1,6 @@
 package com.back;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,6 +8,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -59,7 +61,44 @@ public class QuestionRepositoryTests {
         List<Question> all = this.questionRepository.findAll();
         assertEquals(2, all.size());
 
+        System.out.println(all.size());
+
         Question q = all.get(0);
         assertEquals("sbb가 무엇인가요?", q.getSubject());
+
+        System.out.println(q.getSubject());
+    }
+
+    @Test
+    void t2() {
+        Question q1 = questionRepository.findById(1).get(); //Optional 객체로 반환 -> get()
+
+//        assertEquals("sbb가 무엇인가요?", q1.getSubject());
+        assertThat(q1.getSubject()).isEqualTo("sbb가 무엇인가요?");
+    }
+
+    @Test
+    void t3() {
+        /**유저가 임의로 생성하는 변수들은 JPA가 미리 알아서 생성할 수 없다!! */
+        Question q1 = questionRepository.findBySubject("sbb가 무엇인가요?").get();
+        // select * from question where subject = 'sbb가 무엇인가요?'
+
+        assertThat(q1.getId()).isEqualTo(1);
+        assertThat(q1.getContent()).isEqualTo("sbb에 대해서 알고 싶습니다.");
+
+    }
+
+    @Test
+    @DisplayName("질문 수정")
+    void t4() {
+        Question q1 = questionRepository.findById(1).get();
+
+        q1.setSubject("sbb가 무엇인가요? - 수정"); // 객체 수준에서 변경
+        questionRepository.save(q1);  // 실제 DB에 반영은 save() 로 진행
+        questionRepository.flush();   // 변경 내용을 DB에 즉시 반영
+
+        Question q1_2 = questionRepository.findById(1).get();
+        assertThat(q1.getSubject()).isEqualTo("sbb가 무엇인가요? - 수정");
+
     }
 }
