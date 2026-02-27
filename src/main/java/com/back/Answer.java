10f4362c0ff0cd8d1873ae37b732696707fbaa31
perandowind <perandowind@gmail.com>
2026-02-27 10:21:25 +0900

    7

---
+++ src/main/java/com/back/Answer.java
@@ -0,0 +1,16 @@
+package com.back;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String content;
+    /**현재 내 클래스가 앞에옴 -> Many*/
+    @ManyToOne
+    private Question question;
+}
